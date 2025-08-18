8017c4392310a3bfba19acd9998f479541eef8d3
MNNLRAbhijitMaske <amaske311@gmail.com>
2025-08-18 13:24:42 +0530

    change metastructure 3

--- backend/controllers/metaDatashareController.js
+++ backend/controllers/metaDatashareController.js
@@ -18,12 +18,13 @@ exports.shareMetaData = async (req, res) => {
         <meta property="og:site_name" content="U Tech" />
         <meta property="og:title" content="${data.title}" />
         <meta property="og:description" content="${data.description}" />
-        <meta property="og:image" content="${data.image}" />
-        <meta property="og:image:secure_url" content="${data.image}" />
-        <meta property="og:image:alt" content="${data.title}" />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
+       <meta property="og:image" content="${data.image}" />
+<meta property="og:image:secure_url" content="${data.image}" />
+<meta property="og:image:alt" content="${data.title}" />
+<meta property="og:image:type" content="image/jpeg" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="627" />
+
         <meta property="og:type" content="website" />
         <meta property="og:url" content="${data.url}" />
         <meta property="og:locale" content="en_US" />
