927b6b3c97613945d4f354122929e6457adfbcb7
MNNLRAbhijitMaske <amaske311@gmail.com>
2025-08-21 11:58:34 +0530

    create nussinessanalitics page

---
+++ frontend/src/pages/solutions/bi-analytics/Data.jsx
@@ -0,0 +1,312 @@
+export const biServices = [
+    {
+        title: "Custom BI Development",
+        description:
+            "As a BI developer, we provide comprehensive business intelligence development. We use Online Analytical Processing (OLAP) technology to facilitate multi-dimensional analyses of big data from multiple sources.",
+        icon: "/assets/solutions/bi-analytics/icons/10001.svg",
+    },
+    {
+        title: "BI Data Warehouse Development",
+        description:
+            "We provide Business Intelligence (BI) data warehouse services, including the engineering of central repositories, to Extract, Transform and Load (ETL) data from disparate platforms and applications.",
+        icon: "/assets/solutions/bi-analytics/icons/10002.svg",
+    },
+    {
+        title: "On-Premise BI Software Development",
+        description:
+            "We develop self-service and server-based dashboard software that can be easily deployed and installed into any on-premise network environment. Our BI solutions can be customized for any existing hardware.",
+        icon: "/assets/solutions/bi-analytics/icons/10003.svg",
+    }
+];
+
+export const biNewServices = [
+    {
+        title: "BI Reporting & Dashboards",
+        description:
+            "As a BI report developer, we build robust and customizable dashboards with reporting and data analytics tools that provide real-time insights in a variety of pixel-perfect displays. Our dashboards and reporting tools have the ability to run on any browser or platform.",
+        icon: "/assets/solutions/bi-analytics/icons/10004.svg",
+    },
+    {
+        title: "Data Science & Machine Learning",
+        description:
+            "We engineer data science and machine learning tools to configure data and create predictive models with the ability to be shared and custom-scripted. Our machine learning models are able to be automatically trained based on data.",
+        icon: "/assets/solutions/bi-analytics/icons/10005.svg",
+    },
+    {
+        title: "Cloud BI Development",
+        description:
+            "We deploy cloud-based applications using container architecture for lightweight app development. We ensure industry compliance, governance, and security protocols are built into all of the business intelligence technologies we develop.",
+        icon: "/assets/solutions/bi-analytics/icons/10006.svg",
+    }
+];
+
+export const cloudSolutions = [
+    {
+        title: "AI Cloud Solutions",
+        description:
+            "We leverage the power of custom AI solutions to create cloud machine learning platforms, launch intelligent chatbots, optimize external data storage, and so much more.",
+        icon: "/assets/solutions/bi-analytics/icons/10007.svg",
+    },
+    {
+        title: "IoT Cloud Solutions",
+        description:
+            "We combine the power of IoT and the cloud to create robust, highly functional cloud IoT solutions that support connected applications, power industrial operations, and more.",
+        icon: "/assets/solutions/bi-analytics/icons/10008.svg",
+    },
+    {
+        title: "Big Data Cloud Solutions",
+        description:
+            "We unlock the power of cloud computing to enhance big data storage processing & visualization, adjusting big data volume, velocity, and variety to meet business needs.",
+        icon: "/assets/solutions/bi-analytics/icons/10009.svg",
+    }
+];
+
+export const aiBusinessIntelligence = [
+    {
+        title: "Artificial Intelligence with Power BI",
+        description:
+            "Chetu’s integration specialists leverage AI-powered algorithms within Power BI to enhance decision-making processes and extract deeper data insights, unlocking nuances and patterns to optimize data processing utilized to drive innovation and modernize operations. Boosting efficiency and reducing expenses makes Power BI infused with AI a versatile, modern tool to propel business intelligence forward.",
+        icon: "/assets/solutions/bi-analytics/icons/10010.svg",
+    },
+    {
+        title: "AI in Business Analytics",
+        description:
+            "Our analytics experts harness AI’s power to give end-users unprecedented insights into business operations, streamlined workflow, automated business and data intelligence, and detailed marketing campaign performance reporting. Deploying AI-powered analytics enables accurate risk assessment, error-free data processing and extraction, and unprecedented ability to drive efficiency and facilitate innovation.",
+        icon: "/assets/solutions/bi-analytics/icons/10011.svg",
+    }
+];
+
+export const biAnalyticsSolutions = [
+    {
+        title: "Custom BI Software Solutions",
+        description:
+            "We build custom BI solutions for desktops, mobile devices, and browser apps, complete with personalized and customizable dashboards that offer real-time visibility on KPIs. We include features like big data processing, predictive analytics, and much more.",
+        icon: "/assets/solutions/bi-analytics/icons/10012.svg",
+    },
+    {
+        title: "Sales Analytics Solutions",
+        description:
+            "Our custom sales analytics solutions provide accurate, scalable, and real-time sales forecasting, enabling users to connect to any sales data source, automatically populate QBR dashboards, and send/receive customizable notifications/alerts from any device.",
+        icon: "/assets/solutions/bi-analytics/icons/10013.svg",
+    },
+    {
+        title: "Marketing Analytics Solutions",
+        description:
+            "We create Marketing Analytics Solutions to maximize ROI by combining marketing data with sales and finance data to clearly reflect the impact of marketing efforts across departments with API connections to any marketing data source.",
+        icon: "/assets/solutions/bi-analytics/icons/10014.svg",
+    },
+    {
+        title: "Finance Analytics Solutions",
+        description:
+            "We provide Finance Analytics Solutions that consolidate KPI’s and provide real-time metrics around revenue, profitability, and productivity. We create custom apps and data science models to connect teams and provide action-oriented data.",
+        icon: "/assets/solutions/bi-analytics/icons/10015.svg",
+    },
+    {
+        title: "IT & Helpdesk Analytics Solutions",
+        description:
+            "We create custom IT & helpdesk analytics solutions, seamlessly integrated with your existing IT infrastructure, to enable business users to securely store data with personalized governing capabilities (RBAC modules) to scale access and provide fast & centralized query responses.",
+        icon: "/assets/solutions/bi-analytics/icons/10016.svg",
+    },
+    {
+        title: "Social Media Analytics Solutions",
+        description:
+            "Our custom social media analytics solutions provide users with the ability to analyze, monitor, modify, and visualize market trends in real-time, as well as track their social media campaigns and performance analytics across multiple channels from a single, unified platform.",
+        icon: "/assets/solutions/bi-analytics/icons/10017.svg",
+    }
+];
+
+export const biExtraSolutions = [
+    {
+        title: "Data Preparation & Integrations",
+        description:
+            "We help you acquire data from diverse sources and streamline it into one secure hub with the ability to integrate with any existing infrastructure for optimized analysis.",
+        icon: "/assets/solutions/bi-analytics/icons/10018.svg",
+    },
+    {
+        title: "Mobile BI Applications",
+        description:
+            "We build custom mobile apps designed to promote enhanced scalability, better connectivity & broadband coverage, unlimited data storage, and so much more.",
+        icon: "/assets/solutions/bi-analytics/icons/10019.svg",
+    },
+    {
+        title: "Real-Time Visual Data Analysis",
+        description:
+            "We create custom interactive visual analytics dashboards for real-time data analysis and monitoring through graphs, tables, charts, and more on an easy-to-use interface.",
+        icon: "/assets/solutions/bi-analytics/icons/10020.svg",
+    }
+];
+
+export const biAdvancedSolutions = [
+    {
+        title: "Augmented Analytics",
+        description:
+            "Our developers augment your existing analytics software with AI and other tools to deliver broad and precise insights and instantly detect anomalies.",
+        icon: "/assets/solutions/bi-analytics/icons/10021.svg",
+    },
+    {
+        title: "Unified Business Insights",
+        description:
+            "Our developers help you connect KPI’s, dashboards, and reports in a secure and easy-to-use portal, creating a fuller scope of unified analytics.",
+        icon: "/assets/solutions/bi-analytics/icons/10022.svg",
+    },
+    {
+        title: "Automated Business Processes",
+        description:
+            "We implement features that automate and standardize the development process to improve productivity, save time, and reduce costs.",
+        icon: "/assets/solutions/bi-analytics/icons/10023.svg",
+    },
+    {
+        title: "Embedded BI Analytics",
+        description:
+            "We create embedded analytics platforms to seamlessly integrate with any application or self-service portal using API & SAML authentication.",
+        icon: "/assets/solutions/bi-analytics/icons/10024.svg",
+    },
+    {
+        title: "Security Compliance & Governance",
+        description:
+            "We implement SecureKey Authorization technology to provide enhanced security and the ability to govern accessibility permissions on a scalable level.",
+        icon: "/assets/solutions/bi-analytics/icons/10025.svg",
+    },
+    {
+        title: "Flexible Data Deployment",
+        description:
+            "Our consultant BI experts help you deploy data to an in-cloud, on-premise, or hybrid platform to meet your specific data deployment needs.",
+        icon: "/assets/solutions/bi-analytics/icons/10026.svg",
+    }
+];
+
+export const biOutsourcingServices = [
+    {
+        title: "Business Intelligence Outsourcing",
+        description:
+            "Unlock unparalleled efficiency and strategic focus by entrusting your business intelligence tasks to our expert team. Our experts streamline processes, optimize workflows, and proactively manage the intricacies of your data landscape. We offload technical management to ensure you can focus on core business functions, fostering innovation and driving growth.",
+        icon: "/assets/solutions/bi-analytics/icons/10027.svg",
+    },
+    {
+        title: "Business Intelligence Integration Services",
+        description:
+            "We revolutionize data accessibility throughout your organization with our unified approach. We provide real-time connectivity, user-friendly interfaces, and scalable solutions for your business needs. We enable seamless integration that guarantees your team’s instant access to data-driven insights.",
+        icon: "/assets/solutions/bi-analytics/icons/10028.svg",
+    },
+    {
+        title: "Business Intelligence Implementation Services",
+        description:
+            "We begin with a needs assessment, ensuring a tailored strategy aligns with business needs. We integrate diverse data sources. We design customized solutions and provide comprehensive training. Our continuous support and optimization services ensure your BI system evolves with your business.",
+        icon: "/assets/solutions/bi-analytics/icons/10029.svg",
+    },
+    {
+        title: "Business Intelligence Data Integration",
+        description:
+            "Business intelligence data integration is crucial as it merges disparate data sources to create a unified view for informed decision-making with fine-tuned accuracy. We bridge the gap between data sources, enabling businesses to unlock meaningful insights.",
+        icon: "/assets/solutions/bi-analytics/icons/10030.svg",
+    }
+];
+export const biPlatformIntegrations = [
+    {
+        title: "ZOHO Analytics",
+        description:
+            "Our developers integrate the ZOHO analytics platform with your current business systems to collect raw data and transform it into visually stunning and insightful reports and dashboards.",
+        icon: "/assets/solutions/aws/icons/10028.svg",
+    },
+    {
+        title: "Oracle BI",
+        description:
+            "Our talented developers seamlessly integrate with Oracle Business Intelligence solutions to help businesses effectively manage their business insights, streamline workflows, and gain full visibility into their metrics.",
+        icon: "/assets/solutions/aws/icons/10029.svg",
+    },
+    {
+        title: "Logi Analytics",
+        description:
+            "We integrate Logi Analytics Solutions with your existing business systems to help cut down on maintenance and deployment costs while allowing you to utilize a fully documented embedded analytics platform.",
+        icon: "/assets/solutions/aws/icons/10030.svg",
+    },
+    {
+        title: "SAP Business Objects",
+        description:
+            "Chetu can improve business efficiency by integrating any third-party software with SAP BusinessObjects, put in place to elevate your business processes/workflows for real-time reporting and increased ROI.",
+        icon: "/assets/solutions/aws/icons/10031.svg",
+    },
+    {
+        title: "Looker",
+        description:
+            "We can integrate Looker with any application and business system in your organization’s infrastructure, enabling users to explore, analyze, and share real-time, insightful business analytics with ease.",
+        icon: "/assets/solutions/aws/icons/10027.svg",
+    },
+    {
+        title: "DOMO",
+        description:
+            "By integrating DOMO with your current business applications, you can pull Excel spreadsheets, files attached to emails, files from FTP, flat files, and more into a single, cloud-based platform.",
+        icon: "/assets/solutions/aws/icons/10026.svg",
+    },
+    {
+        title: "BIRT",
+        description:
+            "Our company specializes in customized BIRT application development, empowering you with full visibility into metrics and informed decision-making. Seamlessly integrate BIRT software into your systems, transforming raw data into visually appealing reports and dashboards.",
+        icon: "/assets/solutions/aws/icons/10025.svg",
+    },
+    {
+        title: "Power BI",
+        description:
+            "Leverage Power BI for data analysis. Seamlessly integrate with processes, unlocking insights. Our custom development delivers real-time analytics. Understand data, optimize decisions, drive growth with our Power BI developers.",
+        icon: "/assets/solutions/aws/icons/10024.svg",
+    },
+    {
+        title: "Tableau",
+        description:
+            "Unlock your data's potential with our seamless Tableau dashboard development. Our Tableau developer services transform raw data into stunning dashboards for actionable insights. Stay ahead in today's business landscape by easily exploring real-time data and making data-driven decisions confidently.",
+        icon: "/assets/solutions/aws/icons/10023.svg",
+    }
+];
+export const Faqs = [
+    {
+        question: "What are business intelligence systems?",
+        answer:
+            "Business Intelligence systems are crafted to ingest and analyze business data, displaying it in user-friendly structures such as reports, dashboards, charts, and graphs. This empowers users to acquire actionable insights and render informed decisions based on the scrutinized information.",
+    },
+    {
+        question: "What are the benefits of implementing BI software solutions?",
+        answer:
+            "Implementing BI software solutions brings benefits like improved decision-making through data-driven insights, enhanced operational efficiency, better forecasting and trend analysis, streamlined data integration, and real-time identification of business opportunities and trends.",
+    },
+    {
+        question: "How are the security and governance of BI software solutions ensured?",
+        answer:
+            "Security and governance of BI software solutions are prioritized. Implementing SecureKey Authorization technology and offering flexible data deployment options (in-cloud, on-premise, or hybrid) ensure enhanced security and scalability.",
+    },
+    {
+        question: "What is the role of artificial intelligence (AI) in business intelligence?",
+        answer:
+            "Artificial intelligence (AI) refers to utilizing computer systems to emulate diverse aspects of human intelligence, including problem-solving, learning, and decision-making. While still in its early stages of technological development, businesses recognize the immense potential of AI in areas such as speech recognition, decision-making processes, and a wide range of other applications.",
+    }
+];
+
+export const portfolioItems = [
+    {
+        title: "Our Portfolio",
+        img: "/assets/solutions/bi-analytics/10036.jpg",
+
+    },
+    {
+        title: "Top 3 Reasons to Upgrade to Cognos Business Intelligence Software ",
+        img: "/assets/solutions/bi-analytics/10037.jpg",
+    },
+    {
+        title: "6 Must-Have Features of a Good BI Analytics Platform",
+        img: "/assets/solutions/bi-analytics/10038.jpg",
+    },
+    {
+        title: "Business Intelligence Software Development for Retailers",
+        img: "/assets/solutions/bi-analytics/10039.jpg",
+
+    },
+    {
+        title: "SAP BusinessObjects and Intelligence Solutions",
+        img: "/assets/solutions/bi-analytics/10040.jpg",
+    },
+    {
+        title: "U Tech ANNOUNCES AWS SELECT PARTNER STATUS",
+        img: "/assets/solutions/bi-analytics/10041.jpg",
+    },
+
+];
+
